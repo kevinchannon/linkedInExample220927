42d4417a5cbb17f88ef8093e63c9586370f31129
kevin <kevinchannon@gmail.com>
2022-09-27 07:36:04 +0100

    Use std::generate instead of raw loop for getting lines from file

--- linkedInExample220927/linkedInExample220927.cpp
+++ linkedInExample220927/linkedInExample220927.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 size_t calculateItemCount(std::ifstream& file) {
   file.seekg(0, std::ifstream::end);
@@ -12,30 +13,31 @@ size_t calculateItemCount(std::ifstream& file) {
   return (byteCount / 9) + (byteCount % 9 != 0 ? 1 : 0);
 }
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+  std::cout << "Hello World!\n";
 
-    constexpr auto fileName = R"(c:\Jeux\AJL\D7.txt)";
+  constexpr auto fileName = R"(c:\Jeux\AJL\D7.txt)";
 
-    std::string myText;
-    std::ifstream myReadFile(fileName);
-    if (myReadFile.is_open()) {
-      std::cout << "Error opening '" << fileName << "'\n";
-      return 1;
-    }
+  std::ifstream myReadFile(fileName);
+  if (myReadFile.is_open()) {
+    std::cout << "Error opening '" << fileName << "'\n";
+    return 1;
+  }
 
-    auto strMots7 =
-        std::vector<std::string>(calculateItemCount(myReadFile), std::string{});
+  auto strMots7 =
+      std::vector<std::string>(calculateItemCount(myReadFile), std::string{});
 
-    int i = 0;
-    while (getline(myReadFile, myText)) {
-      strMots7[i] = myText;
-      i++;
-    }
+  std::string myText;
+  const auto getLineFromFile = [&myReadFile, &myText]() {
+    std::getline(myReadFile, myText);
+    return myText;
+  };
 
-    std::cout << strMots7[i - 1] << std::endl;
-    std::cout << "Fin";
-    
-    return 0;
+  std::generate(strMots7.begin(), strMots7.end(), getLineFromFile);
+
+  std::cout << strMots7.back() << std::endl;
+
+  std::cout << "Fin";
+
+  return 0;
 }
