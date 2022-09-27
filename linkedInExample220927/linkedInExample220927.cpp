0d57a7b4572fb0b180444a11edab1b2d78b08919
kevin <kevinchannon@gmail.com>
2022-09-27 07:13:07 +0100

    Add some vertical whitespace to improve readability

--- linkedInExample220927/linkedInExample220927.cpp
+++ linkedInExample220927/linkedInExample220927.cpp
@@ -6,24 +6,30 @@
 int main()
 {
     std::cout << "Hello World!\n";
+
     std::string myText;
     std::ifstream MyReadFile("c:\\Jeux\\AJL\\D7.txt");
     if ((MyReadFile.rdstate() & std::ifstream::failbit)) {
       std::cout << "Error opening 'C:\\Jeux\\AJL\\D7.txt'\n";
       return 1;
     }
+
     MyReadFile.seekg(0, MyReadFile.end);
     int nombre = (int)(ceil)((double)(unsigned long int)MyReadFile.tellg() / 9.);
     std::string* strMots7 = new std::string[nombre];
     MyReadFile.seekg(0);
+
     int i = 0;
     while (getline(MyReadFile, myText)) {
       strMots7[i] = myText;
       i++;
     }
+
     std::cout << strMots7[i - 1] << std::endl;
+    
     MyReadFile.close();
     delete[] strMots7;
     std::cout << "Fin";
+    
     return 0;
 }
