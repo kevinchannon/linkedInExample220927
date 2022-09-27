d3973778c522c92be054c9fc352491656801db79
kevin <kevinchannon@gmail.com>
2022-09-27 07:12:31 +0100

    Remove global 'using namespace std'

--- linkedInExample220927/linkedInExample220927.cpp
+++ linkedInExample220927/linkedInExample220927.cpp
@@ -3,29 +3,27 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int main()
 {
-    cout << "Hello World!\n";
-    string myText;
-    ifstream MyReadFile("c:\\Jeux\\AJL\\D7.txt");
-    if ((MyReadFile.rdstate() & ifstream::failbit)) {
-      cout << "Error opening 'C:\\Jeux\\AJL\\D7.txt'\n";
+    std::cout << "Hello World!\n";
+    std::string myText;
+    std::ifstream MyReadFile("c:\\Jeux\\AJL\\D7.txt");
+    if ((MyReadFile.rdstate() & std::ifstream::failbit)) {
+      std::cout << "Error opening 'C:\\Jeux\\AJL\\D7.txt'\n";
       return 1;
     }
     MyReadFile.seekg(0, MyReadFile.end);
     int nombre = (int)(ceil)((double)(unsigned long int)MyReadFile.tellg() / 9.);
-    string* strMots7 = new string[nombre];
+    std::string* strMots7 = new std::string[nombre];
     MyReadFile.seekg(0);
     int i = 0;
     while (getline(MyReadFile, myText)) {
       strMots7[i] = myText;
       i++;
     }
-    cout << strMots7[i - 1] << endl;
+    std::cout << strMots7[i - 1] << std::endl;
     MyReadFile.close();
     delete[] strMots7;
-    cout << "Fin";
+    std::cout << "Fin";
     return 0;
 }
